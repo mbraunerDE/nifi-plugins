107b5528b92e9a82eda5660eec0ac8e13b990b3f
Max Brauner <mbrauner@acardo.com>
2022-07-12 14:18:47 +0200

    Problem: found an ms-teams processor, but body via content is not possible
    Solution: copy and modify it

---
+++ nifi-mbrauner-plugins-processors/src/main/java/teams/processors/nifi_notify_teams/Message.java
@@ -0,0 +1,32 @@
+/**
+ * everything based on https://github.com/Bentipe/nifi-NotifyTeams
+ */
+package teams.processors.nifi_notify_teams;
+
+public class Message {
+
+    private String title;
+    private String text;
+
+    public Message(String title, String text) {
+        this.title = title;
+        this.text = text;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+}
